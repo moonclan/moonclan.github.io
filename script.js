2e0df606c4fbcfeb2a67c4dceba38d106be15891
moonclan <119791534+moonclan@users.noreply.github.com>
2022-12-05 12:05:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let date = new Date()
+let month = date.getMonth();
+//определение сезона
+  if (month == 11 || month == 0 || month == 1) {
+    document.body.style = "background-image: url('https://i.ibb.co/JzJTnRS/image.png');"
+  }
+  if (month == 2 || month == 3 || month == 4) {
+    document.body.style = "background-image: url('https://i.ibb.co/pKs8Th2/imgonline-com-ua-Texture-seamless-czu-Z51-H5k-H.png');"
+  }
+  if (month == 5 || month == 6 || month == 7) {
+    document.body.style = "background-image: url('https://i.ibb.co/DVv3BYN/image.png');"
+  }
+  if (month == 8 || month == 9 || month == 10) {
+    document.body.style = "background-image: url('https://i.ibb.co/LSLVLtr/imgonline-com-ua-Texture-seamless-W1u5zur-HKys.png');"
+  }
