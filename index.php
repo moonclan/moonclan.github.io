74717a5f7ed578e2d32172bc91995c56322da530
moonclan <119791534+moonclan@users.noreply.github.com>
2022-12-13 14:46:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$favorite_color = "violet";
+$favorite_number = "82";
+$united_string = "Мой любимый цвет - " . $favorite_color . ", а любимое число - " . $favorite_number;
+print($united_string);
+?>
