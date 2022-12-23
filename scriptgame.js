f496bd724ec729703ff32e52ded8cf4824396d25
moonclan <119791534+moonclan@users.noreply.github.com>
2022-12-23 22:01:28 +0700

    Update scriptgame.js

--- scriptgame.js
+++ scriptgame.js
@@ -1,8 +1,8 @@
 const SCALE = 2;
 const WIDTH = 75;
 const HEIGHT = 80;
-const SCALED_WIDTH = 50;
-const SCALED_HEIGHT = 50;
+const SCALED_WIDTH = 35;
+const SCALED_HEIGHT = 35;
 const CYCLE_LOOP = [0, 1, 0, 2];
 const FACING_DOWN = 0;
 const FACING_UP = 1;
