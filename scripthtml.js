855cee98bbce22eafb918c7e2e78d95c16334fe1
moonclan <119791534+moonclan@users.noreply.github.com>
2023-01-03 21:04:37 +0700

    Update scripthtml.js

--- scripthtml.js
+++ scripthtml.js
@@ -1,7 +1,7 @@
 //скрипты для кнопок и т.п.
 function rasr() {
 var pp = document.getElementById('dp')
-localStorage.setItem('cont', 'display: none;')
-pp.style=localStorage.getItem('cont')
+localStorage.setItem('conta', 'display: none;')
+pp.style=localStorage.getItem('conta')
 
 }
